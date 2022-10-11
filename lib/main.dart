6074547bd6c9a954a64914494a35d5e1fdcab065
SebastianPareira <sebastianpareira911@gmail.com>
2022-10-11 20:01:48 +0200

    second

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
           title: const Text('Welcome to Flutter'),
         ),
         body: const Center(
-          child: Text('Hello, Flutter для початківців'),
+          child: Text('Hello World for Flutter для початківців'),
         ),
       ),
     );
